ba619e28728643387d410394a0519e5dce8edb5d
Ryan State <rwstate@gmail.com>
2019-10-09 16:40:21 -0400

    finish stretch

--- src/App.js
+++ src/App.js
@@ -7,17 +7,21 @@ import axios from "axios";
 
 function App() {
   const [data, setData] = useState({});
+  const [newDate, setNewDate] = useState("2019-10-08");
+  const [submit, setSubmit] = useState(true)
+
   useEffect(() => {
     axios
-      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-      .then(data => {setData(data.data); console.log(data.data)})
+      .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${newDate}`)
+      .then(data => {setData(data.data)})
       .catch(err => console.log(err))
-  }, []);
+  }, [submit]);
 
   return (
     <div className="App">
       <Image data={data}/>
-      <Header data={data} />
+      <Header data={data} newDate={newDate} setNewDate={setNewDate}
+      setSubmit={setSubmit} submit={submit}/>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 const Header = (props) => {
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+    }
     return( 
         <div>
             <h1>{props.data.title}</h1>
             <h3>{props.data.date}</h3>
             <p>{props.data.explanation}</p>
+            <form onSubmit={handleSubmit}>
+                Change Date (MM-DD-YYYY)
+                <input type="text" name="test" value={props.newDate} 
+                    onChange={e => props.setNewDate(e.target.value)}>
+                </input>
+                <input type="submit" onClick={() => props.setSubmit(!props.submit)} ></input>
+            </form>
         </div>
     );
 };
